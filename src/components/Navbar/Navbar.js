1054dd9d958b50cafe80c4d4937147a619799b21
Quang Huy <quanghuytran172@gmail.com>
2022-01-08 14:23:23 +0700

    Make reponsive website

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/img/logo.png";
 
 const Navbar = () => {
+    const [menuOpen, toggleMenu] = useState(false);
+
     return (
         <div className='header'>
             <div className='nav'>
                 <img src={logo} alt='zxc' />
-                <ul>
+                <ul className={menuOpen ? "active" : ""}>
                     <li>
                         <a href='#!'>INTRODUCTION</a>
                     </li>
@@ -27,6 +29,19 @@ const Navbar = () => {
                         <a href='#!'>APPLY FOR FREE USE</a>
                     </li>
                 </ul>
+
+                <button
+                    className={menuOpen ? "active" : ""}
+                    onClick={() => toggleMenu(!menuOpen)}
+                >
+                    <ion-icon name={menuOpen ? "close-outline" : "menu"} />
+                </button>
+                <button
+                    className={!menuOpen ? "active button2" : "button2"}
+                    onClick={() => toggleMenu(!menuOpen)}
+                >
+                    <ion-icon name='close-outline' />
+                </button>
             </div>
         </div>
     );
